92d1399e1d01fb864afdffcefbc620826b9c51a3
andron <andrejjchabanjuk@rambler.ru>
2025-03-20 23:37:05 +0300

    start

---
+++ pizza/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <h1>Home
+      <Button>кнопка</Button>
+    </h1>
+  )
+}
